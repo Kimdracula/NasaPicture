ca5e1e5207f3dd37af095ecb2ae4b6395ef9bece
Tyler <vsnow150290@gmail.com>
2022-06-05 18:36:42 +0300

    Сделал переключение тем

--- app/src/main/java/com/homework/nasapicture/ui/main/MainActivity.kt
+++ app/src/main/java/com/homework/nasapicture/ui/main/MainActivity.kt
@@ -3,17 +3,34 @@ package com.homework.nasapicture.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.homework.nasapicture.R
+import com.homework.nasapicture.utils.KEY_NEW_THEME
+import com.homework.nasapicture.utils.KEY_SP
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_Dark)
         super.onCreate(savedInstanceState)
+        setTheme(getNewTheme())
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNowAllowingStateLoss()
         }
+    }
+
+    fun getNewTheme(): Int {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_NEW_THEME, -1)
+    }
+
+    fun setNewTheme(currentTheme: Int) {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_NEW_THEME, currentTheme)
+        editor.apply()
+        recreate()
     }
 }
--- app/src/main/java/com/homework/nasapicture/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/homework/nasapicture/ui/settings/SettingsFragment.kt
@@ -1,15 +1,17 @@
 package com.homework.nasapicture.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.homework.nasapicture.R
 import com.homework.nasapicture.databinding.FragmentSettingsBinding
-import com.homework.nasapicture.ui.main.MainFragment
+import com.homework.nasapicture.ui.main.MainActivity
 
 class SettingsFragment : Fragment() {
-
+    private lateinit var mainActivity: MainActivity
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
@@ -26,11 +28,45 @@ class SettingsFragment : Fragment() {
 
     companion object {
         fun newInstance() = SettingsFragment()
-        private var isMain = true
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        when (mainActivity.getNewTheme()) {
+            R.style.Theme_Dark -> binding.switchDarkTheme.isChecked
+        }
+        setTheme()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = requireActivity() as MainActivity
+    }
+
+
+    private fun setTheme() {
+        with(binding) {
+            switchDarkTheme.setOnClickListener {
+                mainActivity.setNewTheme(R.style.Theme_Dark)
+
+            }
+
+            switchLightTheme.setOnClickListener {
+                mainActivity.setNewTheme(R.style.Theme_NasaPicture)
+
+            }
+
+            switchGreenTheme.setOnClickListener {
+                mainActivity.setNewTheme(R.style.Theme_NasaPicture)
+
+            }
+        }
+    }
+
+
 }
