8a6d9f8a649e182810593ce4f0d763a6c6e7adf6
Tyler <vsnow150290@gmail.com>
2022-06-13 21:12:30 +0300

    Настроил фрагмент Марса

--- app/src/main/java/com/homework/nasapicture/ui/mars/MarsFragment.kt
+++ app/src/main/java/com/homework/nasapicture/ui/mars/MarsFragment.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.homework.nasapicture.R
 import com.homework.nasapicture.databinding.FragmentMarsBinding
 import com.homework.nasapicture.utils.Date
-import com.homework.nasapicture.utils.UNKNOWN_ERROR
 import com.homework.nasapicture.viewmodel.MarsState
 import com.homework.nasapicture.viewmodel.MarsViewModel
-import java.util.*
 
 
 class MarsFragment : Fragment() {
@@ -35,18 +32,16 @@ class MarsFragment : Fragment() {
         viewModel = ViewModelProvider(this)[MarsViewModel::class.java]
         viewModel.getLiveData().observe(viewLifecycleOwner) {
             renderData(it)
-    }
+        }
         val date = Date()
-        viewModel.sendRequest(date.formattedYesterday,"spirit")
+        viewModel.sendRequest("2015-6-3", "spirit")
 
-      binding.datePicker.setOnDateChangedListener { datePicker, year, month, day ->
-viewModel.sendRequest("$year-${month+1}-$day","spirit")
-      }
-
-
-
+        binding.datePicker.setOnDateChangedListener { datePicker, year, month, day ->
+            viewModel.sendRequest("$year-${month + 1}-$day", "spirit")
         }
 
+
+    }
 
 
     private fun renderData(it: MarsState) {
@@ -57,30 +52,48 @@ viewModel.sendRequest("$year-${month+1}-$day","spirit")
             is MarsState.Error -> {
                 with(binding) {
                     imageViewProgress.visibility = View.GONE
-                    binding.marsPictureImageView.load(R.drawable.error_image)}
+                    binding.marsPictureImageView.load(R.drawable.error_image)
+                }
             }
             is MarsState.Success -> {
                 with(binding) {
                     imageViewProgress.visibility = View.GONE
-                    if(it.marsRoverPhotos.photos.isEmpty()){
-                            binding.marsPictureImageView.load(R.drawable.error_image)
-                        }else{
-                    marsPictureImageView.load(it.marsRoverPhotos.photos[0].imgSrc)}
+                    if (it.marsRoverPhotos.photos.isEmpty()) {
+                        binding.marsPictureImageView.load(R.drawable.error_image)
+                    } else {
+                        if (it.marsRoverPhotos.photos[0].imgSrc!=null){
 
-                    textViewTest.text=it.marsRoverPhotos.photos[0].camera.fullName}
+                        marsPictureImageView.load(it.marsRoverPhotos.photos[0].imgSrc)}
+                    }
 
+                    if (it.marsRoverPhotos.photos[0].camera.fullName != null) {
+                        textViewCamera.text =
+                            ("Камера: ${it.marsRoverPhotos.photos[0].camera.fullName}")
+                    }
+
+                    if (it.marsRoverPhotos.photos[0].rover.launchDate != null) {
+                        textViewLaunch.text =
+                            ("Дата запуска: ${it.marsRoverPhotos.photos[0].rover.launchDate}")
+                    }
+
+                    if (it.marsRoverPhotos.photos[0].rover.landingDate != null) {
+                        textViewLanding.text =
+                            ("Дата посадки: ${it.marsRoverPhotos.photos[0].rover.landingDate}")
+                    }
+
+
+                }
             }
         }
     }
 
-
     companion object {
         fun newInstance() = MarsFragment()
-            }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 
-    }
+}
